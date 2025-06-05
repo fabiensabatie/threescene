e0cbd58521b1c58979199ab684917dbb01468713
Fabien Sabatié <fabien.sabatie@live.fr>
2025-06-05 20:10:47 +0200

    three

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ModuleFederationPlugin } = require('webpack').container;
+
+module.exports = {
+  mode: 'development',
+  entry: './index.tsx',
+  devtool: 'inline-source-map',
+  devServer: {
+    port: 3001,
+    open: true,
+    hot: true,
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+    }),
+    new ModuleFederationPlugin({
+      name: 'threescene',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './Block': './App',
+      },
+      // Independent dependencies - no sharing
+    }),
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+}; 
