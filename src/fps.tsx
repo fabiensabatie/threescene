1d26adc8f1975cb39eb2842a5215a33a93e5a6bb
Fabien Sabatié <fabien.sabatie@live.fr>
2025-06-06 05:15:25 +0200

    Update fps.tsx

--- src/fps.tsx
+++ src/fps.tsx
@@ -13,8 +13,8 @@ export default function FpsGame() {
   }
 
   const canvasStyle: React.CSSProperties = {
-    width: '100vw',
-    height: '100vh'
+    width: '100%',
+    height: '100%'
   }
 
   return (
