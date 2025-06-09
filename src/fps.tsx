05d3674220a88e67a50630d7b872ad4b4d900e14
Fabien Sabatié <fabien.sabatie@live.fr>
2025-06-09 15:22:41 +0200

    Update fps.tsx

--- src/fps.tsx
+++ src/fps.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
-import { Stats, Environment, PointerLockControls } from '@react-three/drei'
+import { Environment, PointerLockControls } from '@react-three/drei'
 import { Canvas } from '@react-three/fiber'
 import Game from './Game'
 import Overlay from './Overlay'
-import { Leva } from 'leva'
 
 export default function FpsGame() {
   const rootStyle: React.CSSProperties = {
@@ -36,10 +35,8 @@ export default function FpsGame() {
         <Environment files="https://content.mext.app/courses/68413038bd1af4c76c20a154/model/rustig_koppie_puresky_1k.hdr" background />
         <Game />
         <PointerLockControls />
-        <Stats />
       </Canvas>
       <Overlay />
-      <Leva />
     </div>
   )
 } 
